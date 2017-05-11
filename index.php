b8e42c65cbb16de3a3dd5463553ab800c66aca56
DonMaxi <DonMaxi@10.0.0.105>
2017-05-11 04:52:39 -0400

    Second Day Work

--- index.php
+++ index.php
@@ -7,10 +7,110 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>ICGC :: Restoration Temple, Dumfries, VA. USA</title>
+        <meta name="description" content="International Central Gospel Church, Restoration Temple in Virginia, USA. Where God is Shaping Vision and Influencing Society through Christ." />
+        <meta name="author" content="//efremtech.com">
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+        <meta name="theme-color" name="theme-color" content="#">
+        <meta name="mobile-web-app-capable" content="yes">
+        <base target="_blank">
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootst">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="http://weloveiconfonts.com/api/?family=zocial" />
     </head>
     <body>
-        <h1>This is Restoration Temple</h1>
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navheader navbar-static-top">
+                <a href="http://centralgospelrtva.org">
+                    <img src="images/logo.png" alt="The International Central Gospel Logo" class="img-responsive logo" />
+                </a>
+            </div>
+            <div class="row">
+                <ul class="nav nav-tabs navbar-right">
+                    <li role="presentation" class="active"><a href="#">Home</a></li>
+                    <li role="presentation"><a href="#">About</a></li>
+                    <li role="presentation" class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            Ministries <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li> 
+                            <li><a href="#">Another action</a></li> 
+                            <li><a href="#">Something else here</a></li> 
+                            <li role="separator" class="divider"></li> 
+                            <li><a href="#">Separated link</a></li> 
+                        </ul>
+                    </li>
+                    <li role="presentation"><a href="#">Media & Store</a></li>
+                    <li role="presentation"><a href="#">Reach Us</a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+
+            <div id="carousel" class="carousel slide row" data-ride="carousel">
+                <!-- Indicators -->
+<!--                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>-->
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="images/carousel/2017_backdrop.jpg" alt="..." class="img-responsive center-block img-fluid" />
+                        <div class="carousel-caption">
+                            <h3>Our Year of Leadership</h3>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/carousel/praise.jpg" alt="..." class="img-responsive center-block img-fluid" />
+                        <div class="carousel-caption">
+                            <h3>Praise Team</h3>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/carousel/welcome.jpg" alt="..." class="img-responsive center-block img-fluid" />
+                        <div class="carousel-caption">
+                            <h3>Praise Team</h3>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/carousel/spirit.jpg" alt="..." class="img-responsive center-block img-fluid" />
+                        <div class="carousel-caption">
+                            <h3>Praise Team</h3>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/carousel/children christmas.jpg" alt="..." class="img-responsive center-block img-fluid" />
+                        <div class="carousel-caption">
+                            <h3>Praise Team</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <!-- /.container -->
+
+
         <?php
         // put your code here
         ?>
