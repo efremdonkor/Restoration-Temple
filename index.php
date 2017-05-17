93d6900c80b97207883fa555156ecaf40af50d1a
DonMaxi <DonMaxi@10.0.0.105>
2017-05-17 02:14:20 -0400

    Second Day Work

--- index.php
+++ index.php
@@ -20,6 +20,9 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/bootst">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
 
@@ -27,88 +30,164 @@ and open the template in the editor.
     </head>
     <body>
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navheader navbar-static-top">
-                <a href="http://centralgospelrtva.org">
-                    <img src="images/logo.png" alt="The International Central Gospel Logo" class="img-responsive logo" />
-                </a>
-            </div>
-            <div class="row">
-                <ul class="nav nav-tabs navbar-right">
-                    <li role="presentation" class="active"><a href="#">Home</a></li>
-                    <li role="presentation"><a href="#">About</a></li>
-                    <li role="presentation" class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                            Ministries <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li> 
-                            <li><a href="#">Another action</a></li> 
-                            <li><a href="#">Something else here</a></li> 
-                            <li role="separator" class="divider"></li> 
-                            <li><a href="#">Separated link</a></li> 
+            <!---<img src="images/logo.png" alt="The International Central Gospel Logo" class="img-responsive" />--->
+            <!-- Static navbar -->
+            <nav class="navbar navbar-default" style="margin-bottom: 0px;">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">ICGC</a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="./">Home <span class="sr-only">(current)</span></a></li>
+                            <li><a href="../navbar-static-top/">About</a></li>
+
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ministries <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Postoral</a></li>
+                                    <li><a href="#">Women's Ministry</a></li>
+                                    <li><a href="#">Men's Ministry</a></li>
+                                    <li><a href="#">Young Adults Ministry</a></li>
+                                    <li><a href="#">Music Ministry</a></li>
+                                    <li><a href="#">Host & Hostesses Ministry</a></li>
+                                    <li><a href="#">Media Ministry</a></li>
+                                    <li><a href="#">Children's Ministry</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="../navbar-fixed-top/">Online Media</a></li>
+                            <li><a href="../navbar-fixed-top/">Reach Us</a></li>
                         </ul>
-                    </li>
-                    <li role="presentation"><a href="#">Media & Store</a></li>
-                    <li role="presentation"><a href="#">Reach Us</a></li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+                    </div><!--/.nav-collapse -->
+                </div><!--/.container-fluid -->
+            </nav>
+            <!--- Carousel Here --->
+            <section class="section-white">
+                <!-- <div class="container">-->
 
-            <div id="carousel" class="carousel slide row" data-ride="carousel">
-                <!-- Indicators -->
-<!--                <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                </ol>-->
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                    </ol>
 
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="images/carousel/2017_backdrop.jpg" alt="..." class="img-responsive center-block img-fluid" />
-                        <div class="carousel-caption">
-                            <h3>Our Year of Leadership</h3>
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="images/carousel/welcome.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h2>Heading</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/carousel/2017_backdrop.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h2>Heading</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/carousel/women.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h2>Heading</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/carousel/praise.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h2>Heading</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/carousel/blessings.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h2>Heading</h2>
+                            </div>
                         </div>
                     </div>
-                    <div class="item">
-                        <img src="images/carousel/praise.jpg" alt="..." class="img-responsive center-block img-fluid" />
-                        <div class="carousel-caption">
-                            <h3>Praise Team</h3>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="images/carousel/welcome.jpg" alt="..." class="img-responsive center-block img-fluid" />
-                        <div class="carousel-caption">
-                            <h3>Praise Team</h3>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="images/carousel/spirit.jpg" alt="..." class="img-responsive center-block img-fluid" />
-                        <div class="carousel-caption">
-                            <h3>Praise Team</h3>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="images/carousel/children christmas.jpg" alt="..." class="img-responsive center-block img-fluid" />
-                        <div class="carousel-caption">
-                            <h3>Praise Team</h3>
-                        </div>
-                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
                 </div>
 
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <!--  </div>-->
+            </section>
+            
+    <div class="jumbotron jumbotron-sm" style="background-color:#339966;margin-top:2%;color:white;">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h2" style="margin-top:-2%">
+                    Come Worship with us
+                </h1>
             </div>
         </div>
-        <!-- /.container -->
+    </div>
+
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="well">
+                <h3 style="line-height:20%;"><i class="fa fa-home fa-1x" style="line-height:6%;color:#339966"></i> Address:</h3>               
+                <p style="margin-top:6%;line-height:35%">İzmit/KOCAELİ</p>
+                <br />
+                <br />
+                <h3 style="line-height:20%;"><i class="fa fa-envelope fa-1x" style="line-height:6%;color:#339966"></i> E-Mail Address:</h3>
+                <p style="margin-top:6%;line-height:35%">info@birisi.com</p>
+                <br />
+                <br />
+                <h3 style="line-height:20%;"><i class="fa fa-user fa-1x" style="line-height:6%;color:#339966"></i> Head Pastor:</h3>
+                <p style="margin-top:6%;line-height:35%">Sorumlu Kişi</p>
+                <br />
+                <br />
+                <h3 style="line-height:20%;"><i class="fa fa-yelp fa-1x" style="line-height:6%;color:#339966"></i> Destek Merkezi:</h3>
+                <p style="margin-top:6%;line-height:35%"><a href="siteadresi.com/destek">siteadresi.com/destek</a></p>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96690.80542089987!2d29.864461132544537!3d40.77109282810726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cb4f66644bfb9d%3A0x82690ee7586b7eb9!2zxLB6bWl0LCBLb2NhZWxp!5e0!3m2!1str!2str!4v1480782606579" width="100%" height="430" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+       
+
+            <!-- Social Footer, Colour Matching Icons -->
+
+            <div class="container">
+
+                <h3>Multi-Coloured</h3>
+                <hr>
+                <div class="text-center center-block">
+                    <p class="txt-railway">- Bootsnipp.com -</p>
+                    <br />
+                    <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+                    <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+                    <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+                    <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+                </div>
+                <hr>
+            </div>
+
+
+        </div> <!-- /container -->
+
+
+
+             <!--<img src="images/logo.png" alt="The International Central Gospel Logo" class="img-responsive logo" />-->
+
 
 
         <?php
